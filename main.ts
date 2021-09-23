57a42f18fed87085b264128ff8f7274784b3241e
dylanspin <47520114+dylanspin@users.noreply.github.com>
2021-09-23 14:57:00 +0200

    reset button

--- main.ts
+++ main.ts
@@ -3,12 +3,17 @@ input.onButtonPressed(Button.A, function () {
         counting = 1
     }
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    counting = 0
+    timer = 0
+    basic.clearScreen()
+})
 let timer = 0
 let counting = 0
 let maxTime = 10
 basic.forever(function () {
     if (counting == 1) {
-        if (timer <= maxTime) {
+        if (timer < maxTime) {
             timer = timer + 1
         } else {
             timer = 0
@@ -17,5 +22,13 @@ basic.forever(function () {
             basic.clearScreen()
         }
         basic.showNumber(timer)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            . . . # .
+            . . . . .
+            . . . # .
+            `)
     }
 })
